6a9d4a4eaef6a2385bf71bdb0d500e3f2f8fe098
Eric Brelsford <ebrelsford@gmail.com>
2023-10-30 13:19:06 -0400

    Add yieldratio

--- vacs-map-app/src/stores/cropYields.js
+++ vacs-map-app/src/stores/cropYields.js
@@ -10,11 +10,48 @@ export const useCropYieldsStore = defineStore('cropYields', () => {
   const load = async () => {
     if (loading.value || data.value) return false;
     loading.value = true;
-    data.value = Object.freeze(await d3.csv(getDataUrl('crop-yields-mean-models.csv'), d => {
+
+    let transformedData = await d3.csv(getDataUrl('crop-yields-mean-models.csv'), d => {
       return Object.fromEntries(
         Object.entries(d).map(([k, v]) => ([k, (v && v !== "" ? +v : null)]))
       );
-    }));
+    });
+
+    const yieldKeys = Object.keys(transformedData[0])
+      .filter(k => k.startsWith('yield'));
+
+    transformedData = transformedData.map((d, i) => {
+      const rowWithYields = { ...d };
+
+      yieldKeys.forEach(k => {
+        const [_, crop, timeframe, scenario] = k.split('_');
+        if (timeframe === 'historical') return;
+
+        const historicalKey = [
+          'yield',
+          crop,
+          'historical_ssp370',
+        ].join('_');
+
+        if (!yieldKeys.includes(historicalKey)) return;
+
+        const yieldRatioKey = [
+          'yieldratio',
+          crop,
+          timeframe,
+          scenario
+        ].join('_');
+
+        let yieldRatioValue = null;
+        if (d[k] !== null && d[historicalKey] !== null) yieldRatioValue = d[k] / d[historicalKey];
+
+        rowWithYields[yieldRatioKey] = yieldRatioValue;
+      });
+
+      return rowWithYields;
+    });
+
+    data.value = Object.freeze(transformedData);
   };
 
   return {
