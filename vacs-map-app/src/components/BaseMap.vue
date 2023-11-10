65717b886431692cdbab92760a96dc22d92cec2a
Eric Brelsford <ebrelsford@gmail.com>
2023-11-10 14:13:22 -0500

    Refactor layer and source

--- vacs-map-app/src/components/BaseMap.vue
+++ vacs-map-app/src/components/BaseMap.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="base-map-wrapper">
     <div id="baseMapContainer" class="base-map"></div>
-    <GridOverlay :map="map" :mapReady="mapReady" />
+    <GridSource :id="sourceId" :map="map" :mapReady="mapReady" />
+    <GridOverlay
+      id="grid-layer-1"
+      :color-column="selectedColumn"
+      :color-column-extent="selectedColumnExtent"
+      :color-column-quintiles="selectedColumnQuintiles"
+      :color-diverging="false"
+      :sourceId="sourceId"
+      :map="map"
+      :mapReady="mapReady"
+      />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, shallowRef } from 'vue';
+import { computed, onMounted, ref, shallowRef } from 'vue';
+import { storeToRefs } from 'pinia';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {
@@ -14,10 +25,45 @@ import {
   MAPBOX_GL_ACCESS_TOKEN,
   MAPBOX_STYLE,
 } from '@/constants';
+import { useFiltersStore } from '@/stores/filters';
+import { useCropYieldsStore } from '@/stores/cropYields';
+import GridSource from './GridSource.vue';
 import GridOverlay from './GridOverlay.vue';
 
 const map = shallowRef(null);
 const mapReady = ref(false);
+const sourceId = 'cropGrid';
+
+const cropYieldsStore = useCropYieldsStore();
+const filtersStore = useFiltersStore();
+
+const {
+  selectedCrop,
+  selectedMetric,
+  selectedModel
+} = storeToRefs(filtersStore);
+
+const selectedColumn = computed(() => {
+  if (!selectedMetric.value || !selectedCrop.value || !selectedModel.value) {
+    return null;
+  }
+
+  return [
+    selectedMetric.value,
+    selectedCrop.value,
+    selectedModel.value,
+  ].join('_');
+});
+
+const selectedColumnExtent = computed(() => {
+  if (!selectedColumn.value) return null;
+  return cropYieldsStore.getExtent(selectedColumn.value);
+});
+
+const selectedColumnQuintiles = computed(() => {
+  if (!selectedColumn.value) return null;
+  return cropYieldsStore.getQuintiles(selectedColumn.value);
+});
 
 const initializeMap = () => {
   mapboxgl.accessToken = MAPBOX_GL_ACCESS_TOKEN;
