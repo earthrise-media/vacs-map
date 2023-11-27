c9181e8a7b1b87043477493c138459282d7aab0f
mwbernard <mwbernard9@gmail.com>
2023-11-27 13:48:49 -0800

    Update landing page design, responsiveness

--- vacs-map-app/src/LandingPage.vue
+++ vacs-map-app/src/LandingPage.vue
@@ -2,13 +2,10 @@
   <LayoutOpen>
     <div class="map-wrapper-row">
       <div class="callout">
-        <div class="callout-header">What crops have the most potential in a changing climate?</div>
-        <div class="callout-content">
-          The Vision for Adapted Crops and Soils (VACS) aims to foster more resilient food systems,
-          with an initial focus on the African continent. VACS seeks to boost agricultural
-          productivity and nutrition by developing diverse, climate-resilient crop varieties and
-          building healthy soils. Keep reading
-        </div>
+        <div class="callout-header">{{ copy.header_question }}</div>
+        <span class="callout-content">
+          {{ copy.vacs_short }}
+        </span>
         <button class="go-to-topic" @click="navigate">Find out</button>
       </div>
       <div class="map-wrapper">
@@ -21,11 +18,18 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useContentStore } from './stores/siteContent'
 import LayoutOpen from '@/components/layouts/LayoutOpen.vue'
 import MapHomepage from './components/MapHomepage.vue'
 
 const router = useRouter()
 const navigate = () => router.push('/crops')
+
+const contentStore = useContentStore()
+const {
+  copy
+} = storeToRefs(contentStore)
 </script>
 
 <style scoped>
@@ -48,29 +52,89 @@ const navigate = () => router.push('/crops')
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  padding: 0 5rem;
+  padding: 0 2rem 0 6rem;
   flex-basis: 0;
   flex-grow: 1;
   gap: 2rem;
 }
 
 .callout-header {
-  font-size: 3em;
-  line-height: 1.25em;
+  font-family: var(--font-family-header);
+
+  font-size: 2.875rem;
+  line-height: 120%;
 }
 
-.go-to-topic {
-  appearance: none;
-  border: 1px solid #bfbfbf;
-  background: none;
-  border-radius: 4px;
-  color: var(--white);
-  font-size: 0.9em;
+.callout-content {
+  font-size: 0.9375rem;
 }
 
-.topic-picker {
-  display: block;
-  font-size: 1em;
+button {
+  border: none;
   appearance: none;
+  border-radius: 6.25rem;
+  background: var(--ui-blue);
+  
+  color: var(--black);
+  font-family: Work Sans;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+
+  padding: 0.75rem 1.125rem;
+
+  cursor: pointer;
 }
+
+button:hover {
+  background: var(--ui-blue-hover);
+}
+
+@media only screen and (max-width: 1080px) {
+
+  .callout-header {
+    font-size: 2rem;
+    }
+  .callout-content {
+    font-size: 0.875rem;
+    display: -webkit-box; 
+    -webkit-box-orient: vertical; 
+    -webkit-line-clamp: 8; 
+    overflow-y: hidden;
+    text-overflow: ellipsis;
+  }
+
+  button {
+    font-size: 0.875rem;
+  }
+}
+
+@media only screen and (max-width: 720px) {
+  .map-wrapper-row {
+    flex-direction: column-reverse;
+  }
+
+  .map-wrapper {
+    width: 100%;
+  }
+
+  .callout {
+    padding: 1rem;
+  }
+
+  .callout-header {
+  font-size: 1.375rem;
+  line-height: 120%;
+}
+  .callout-content {
+    font-size: 0.875rem;
+  }
+
+  button {
+    width: 100%;
+    font-size: 1rem;
+  }
+}
+
 </style>
