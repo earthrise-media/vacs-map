7f7ad9a1f3c2d1f9b990f831e4663e3a0f704a39
mwbernard <mwbernard9@gmail.com>
2023-11-27 13:49:01 -0800

    Update top bar

--- vacs-map-app/src/components/OverviewTop.vue
+++ vacs-map-app/src/components/OverviewTop.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="overview-top">
     <div class="row">
-      <div>What crops have the most potential in a changing climate?</div>
-      <NavigationButton :to="backRoute">Go Back</NavigationButton>
+      <div> {{ copy.header_question }}</div>
+      <div class="buttons">
+        <button @click="copyLink" title="Copy URL">
+          <img src="../assets/img/copy-link.svg" alt="copy-link">
+        </button>
+        <NavigationButton :to="backRoute">Back</NavigationButton>
+      </div>
     </div>
   </div>
 </template>
@@ -10,17 +15,29 @@
 <script setup>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useContentStore } from '../stores/siteContent'
 import NavigationButton from '@/components/NavigationButton.vue'
 
 const route = useRoute()
 const backRoute = computed(() => (route.path === '/map-explore' ? '/crops' : '/'))
+
+const contentStore = useContentStore()
+const {
+  copy
+} = storeToRefs(contentStore)
+
+const copyLink = () => {
+  navigator.clipboard.writeText(window.location.href);
+}
 </script>
 
 <style scoped>
 .overview-top {
   display: flex;
   flex-direction: column;
-  font-size: 1.5rem;
+  font-family: var(--font-family-h2);
+  font-size: 1.875rem;
   gap: 0.5rem;
   margin: 1rem var(--page-horizontal-margin);
 }
@@ -31,4 +48,29 @@ const backRoute = computed(() => (route.path === '/map-explore' ? '/crops' : '/'
   justify-content: space-between;
   align-items: center;
 }
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+button {
+  appearance: none;
+  border: none;
+  text-decoration: none;
+
+  padding: 0.625rem 0.75rem;
+  border-radius: 6.25rem;
+  background: var(--dark-gray);
+
+  cursor: pointer;
+}
+
+button:hover {
+  background: var(--dark-gray-hover);
+}
+
 </style>
