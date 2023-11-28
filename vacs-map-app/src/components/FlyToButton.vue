0717f2645b8de109653f1e2ea6957fb7c87be7d6
kelsey-taylor <kelsey@stamen.com>
2023-11-28 15:53:46 -0500

    more flytos

--- vacs-map-app/src/components/FlyToButton.vue
+++ vacs-map-app/src/components/FlyToButton.vue
@@ -22,17 +22,25 @@ const { map } = toRefs(props)
 
 const end1 = {
   // east
-  zoom: 6,
-  center: [38.969, 10.182]
+  zoom: 12,
+  center: [38.969, 10.182],
+  pitch: 45
 };
 
-const end2 = {
+const obliqueEth = {
+  zoom: 7.75,
+  center: [ 36.636, 12.839],
+  pitch: 70,
+  bearing: 141
+}
+
+const north = {
   // north
   zoom: 2.75,
   center: [1.99, 30.11]
 };
 
-const end3 = {
+const west = {
   // west
   zoom: 3.75,
   center: [-4.28, 10.29]
@@ -44,7 +52,7 @@ const end4 = {
   center: [24.82, -23.08]
 };
 
-const end5 = {
+const south = {
   // south
   zoom: 3.75,
   center: [37.19, -18]
@@ -58,8 +66,13 @@ const handleClick = () => {
   });
 
   map.value.flyTo({
-    ...end5, // Fly to the selected target
-    duration: 12000, // Animate over 12 seconds
+    // ...west, // Fly to the selected target
+    // ...south, // Fly to the selected target
+    // ...obliqueEth, // Fly to the selected target
+    ...north, // Fly to the selected target
+    // all scenes are 12s except all crop animation
+    // duration: 12000, // Animate over 12 seconds
+    duration: 120000, // Animate over 2 minutes
     essential: true
   });
 };
