3cafa17c960e64e751a7d46187a44c419ba72e7b
mwbernard <mwbernard9@gmail.com>
2023-11-28 19:27:41 -0800

    Make sure pages load

--- vacs-map-app/src/App.vue
+++ vacs-map-app/src/App.vue
@@ -8,14 +8,22 @@
 import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
 import { useCropYieldsStore } from '@/stores/cropYields'
-import { useCropInformationStore } from './stores/cropInformation'
+import { useCropInformationStore } from '@/stores/cropInformation'
+import { useGridStore } from '@/stores/grid'
+import { useFiltersStore } from '@/stores/filters'
 
 const cropYieldsStore = useCropYieldsStore()
 const cropInformationStore = useCropInformationStore()
+const gridStore = useGridStore()
+const filtersStore = useFiltersStore()
 
 onMounted(() => {
+  gridStore.load()
   cropYieldsStore.load()
   cropInformationStore.load()
+
+  // initialize so pages load
+  filtersStore.selectedCrop = filtersStore.availableCrops[0]
 })
 </script>
 
