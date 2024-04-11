35458358b23f6f62504fbcc9cb6c3cd1b04dd484
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-04-11 12:26:37 -0700

    adding desktop attribution

---
+++ vacs-map-app/src/components/attributionLogo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="attribution-logo">
+    <img src="@/assets/img/logos/agmip_eg_plotline_white.png" alt="attribution Logo" />
+  </div>
+</template>
+
+<style scoped>
+.attribution-logo img {
+  width: 200px;
+  height: auto;
+  /* background-color: pink; */
+}
+@media only screen and (max-width: 720px) {
+  /* don't show */
+  .attribution-logo {
+    display: none;
+  }
+}
+</style>
